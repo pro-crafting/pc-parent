e725be706e3fdcda17518351298471b6259e37d4
Martin Panzer <postremus1996@googlemail.com>
2018-04-22 22:30:45 +0200

    add correctly encrypted variables and codesigning file

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cp cd/.travis.settings.xml $HOME/.m2/settings.xml
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_100e57321d95_key -iv $encrypted_100e57321d95_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_0109d4654fe5_key -iv $encrypted_0109d4654fe5_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
