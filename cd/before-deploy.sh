98a638236ad16294bed158b0b2fd0425f5352fb7
Martin Panzer <postremus1996@googlemail.com>
2018-06-13 11:32:41 +0200

    don't deploy branches anymore

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cp cd/.travis.settings.xml $HOME/.m2/settings.xml
-#if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_0109d4654fe5_key -iv $encrypted_0109d4654fe5_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
-#fi
+fi
